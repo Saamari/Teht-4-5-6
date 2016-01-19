b3201b9b807364f242ca3d82c85b6b6e6b78d854
Saamari <sampsa_kautto@hotmail.com>
2016-01-19 12:01:57 +0200

    Homework 1

---
+++ Teht 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+            vehicle.Name = "Ferrari";
+            vehicle.Speed = 250;
+            vehicle.Tyres = 205;
+            vehicle.PrintData();
+            Console.WriteLine(vehicle.ToString());
+            //Console.WriteLine("Cars Name is =" +vehicle.Name);
+            //Console.WriteLine("Cars speed is =" + vehicle.Speed);
+            //Console.WriteLine("Cars Tyres are =" + vehicle.Tyres);
+
+
+        }
+    }
+}
---
+++ Teht 4/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_4
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle data");
+            Console.WriteLine(" - name: " + Name);
+            Console.WriteLine(" - speed: " + Speed);
+            Console.WriteLine(" - tyres: " + Tyres);
+
+        }
+
+             public override string ToString()
+        {
+            
+
+            return " Name " + Name + " Speed " + Speed + " Tyres " + Tyres;
+
+        }
+    }
+}
